c41a5556728752d4d5edafe1937bced8a870c8ee
Valentin Nankov <valentin.nankov@fadata.eu>
2022-06-06 17:32:50 +0300

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request');
+
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=fc0ab497a82a1c99a095b70f4338b7e3&query=${latitude},${longitude}&units=f`
+
+    request({url, json: true}, (error, {body} = {}) => {
+        if (error) {
+            callback('Unable to connect to weather service!')
+        } else if(body.error) {
+            callback('Unable to find location')
+        }
+        else {
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degree out. It feels like ${body.current.feelslike} degrees out`)
+        }
+    })
+}
+
+module.exports = forecast;
